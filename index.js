ba552737330de332c2e65a409b6645f6bd65e56d
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-18 09:21:17 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+var bestCustomer
+function setBestCustomer() {
+  bestCustomer = "not bob"
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+  return bestCustomer
+}
+const leastFavoriteCustomer = "amy"
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "kim"
+  return leastFavoriteCustomer
+}
